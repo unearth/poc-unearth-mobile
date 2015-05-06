3e8335a9ed8820c9cf013aca60d19e8f1f7d6d6b
travisjn <tdizzletrav@gmail.com>
2015-05-06 16:14:15 -0700

    [feature] Add geolocation functionality

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,38 @@
+
+var app = angular.module('unearth', ['ionic', 'leaflet-directive']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+});
+
+app.config(['$stateProvider', '$urlProvider', function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  $stateProvider
+
+  .state('sign-in', {
+    url: '/sign-in',
+    templateUrl: 'templates/sign-in.html'
+  })
+  .state('map', {
+    url: '/map',
+    templateUrl: 'templates/map.html',
+    controller: 'MapController'
+  })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('sign-in');
+
+});
--- www/js/controllers/map-controller.js
+++ www/js/controllers/map-controller.js
@@ -1,22 +1,42 @@
-app.controller('MapController', ['$scope', 'leafletData', function($scope, leafletData) {
-  var layer = L.TileLayer.maskCanvas({
-   radius: 25,               // Radius in pixels or in meters (see useAbsoluteRadius)
-   useAbsoluteRadius: true,  // True: r in meters, false: r in pixels
-   color: '#00000',          // The color of the layer
-   opacity: 0.8,             // Opacity of the fog area
-   noMask: false,            // True results in normal (filled) circled, instead masked circles
-   lineColor: '#A00'         // Color of the circle outline if noMask is true
-  });
+app.controller('MapController', ['$scope', '$interval', 'leafletData', 'Waypoints', '$cordovaGeolocation',
+  function($scope, $interval, leafletData, Waypoints, $cordovaGeolocation) {
 
-  var data = [
-    [37.782651, -122.445900],
-    [37.782745, -122.445700],
-    [37.782842, -122.445500],
-    [37.782919, -122.445300],
-    [37.782992, -122.445100] ];
+    // Set initial view of map on San Francisco with zoom level 15
+    leafletData.getMap().then(function(map) {
+      map.setView(new L.LatLng(40, -150), 15);
+    });
 
-  layer.setData(data);
-  leafletData.getMap().then(function(map) {
-    map.addLayer(layer);
-  });
+    var layer = L.TileLayer.maskCanvas({
+     radius: 25,               // Radius in pixels or in meters of transparent circles (see useAbsoluteRadius)
+     useAbsoluteRadius: true,  // True: r in meters, false: r in pixels
+     color: '#00000',          // The color of the fog layer
+     opacity: 0.8,             // Opacity of the fog area
+     noMask: false,            // True results in normal (filled) circled, false is for transparent circles
+     lineColor: '#A00'         // Color of the circle outline if noMask is true
+    });
+
+    // Waypoints.getWaypoints(function(responseData){
+    //   console.log(responseData);
+      // layer.setData(responseData);
+      // leafletData.getMap().then(function(map) {
+      //   map.addLayer(layer);
+      // });
+    // });
+
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        var lat  = position.coords.latitude;
+        var long = position.coords.longitude;
+        leafletData.getMap().then(function(map) {
+          map.setView(new L.LatLng(lat, long), 15);
+          layer.setData([[lat, long]]);
+          map.addLayer(layer);
+        });
+      }, function(err) {
+        // error
+      });
+
+
 }]);
