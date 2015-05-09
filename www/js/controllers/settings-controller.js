64f2f04696ef662e2f1d915ccd068fa38ecc8777
Sean Conroy <conroysean@gmail.com>
2015-05-09 13:55:25 -0700

    [cleanup] Deletes commented out code

--- www/js/controllers/settings-controller.js
+++ www/js/controllers/settings-controller.js
@@ -6,23 +6,3 @@ angular.module('unearth.settingsController', [])
       $state.go('login');
     };
   });
-
-  //   $scope.credentials = {
-  //     email: '',
-  //     password: ''
-  //   };
-
-  //   $scope.redirectToSignUp = function(){
-  //     $state.go('sign-up');
-  //   }
-
-  //   $scope.login = function(credentials) {
-  //     Authorization.login(credentials.email, credentials.password).then( function (isAuthenticated) {
-  //       if (true) {
-  //         $state.go('tab.map');
-  //       } else {
-  //         $state.go('login');
-  //       }
-  //     });
-  //   };
-  // });
